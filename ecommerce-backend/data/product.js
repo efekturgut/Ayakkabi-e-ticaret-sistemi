081cc94ffe180680f7d5b5d1d9a5851865dfba06
efekturgut <efekaanturgut91@gmail.com>
2026-04-08 11:08:59 +0300

    Add sample product data

---
+++ ecommerce-backend/data/product.js
@@ -0,0 +1,36 @@
+const products = [
+  {
+    id: 1,
+    name: "iPhone 15",
+    price: 50000,
+    stock: 5,
+    category: "telefon",
+    description: "Apple akıllı telefon",
+  },
+  {
+    id: 2,
+    name: "Samsung S24",
+    price: 42000,
+    stock: 8,
+    category: "telefon",
+    description: "Samsung amiral gemisi",
+  },
+  {
+    id: 3,
+    name: "Lenovo Laptop",
+    price: 30000,
+    stock: 4,
+    category: "bilgisayar",
+    description: "16GB RAM laptop",
+  },
+  {
+    id: 4,
+    name: "Logitech Mouse",
+    price: 900,
+    stock: 15,
+    category: "aksesuar",
+    description: "Kablosuz mouse",
+  },
+];
+
+module.exports = products;
